ed43b33aaa49ff04e4fcd0ca5cacf12fa72e1aa3
AslanTabaldiev <tabaldiev0906@gmail.com>
2024-12-09 13:56:44 +0600

    task controller added, window switching added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,5 @@ module org.example.taskmanagementsystem {
 
     opens org.example.taskmanagementsystem.controller to javafx.fxml; // Разрешаем доступ к пакету для JavaFX
     exports org.example.taskmanagementsystem.controller; // Экспортируем пакет контроллеров
+    exports org.example.taskmanagementsystem.model;
 }
